1752cbe4bab7d5e62d949bf5d39aabf7f823fdcb
Victor Andersen <vicand01@easv365.dk>
2026-09-14 12:39:32 +0200

    Test class created for MilkRecipeCalculator

---
+++ SuperChocolateMilk.UnitTests/MilkRecipeCalculatorTests.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperChocolateMilk.UnitTests;
+
+using Xunit;
+using SuperChocolateMilk.Core;
+
+public class MilkRecipeCalculatorTests
+{
+    // Our unit tests will go inside this class!
+}
